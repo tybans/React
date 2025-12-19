1a76c35fc29e14af56e3f92f49fd764dec4f85de
Taiyab Ansari <taiyab0523@gmail.com>
2025-12-19 20:41:01 +0530

    updated

--- Projects/Food_Order/backend/app.js
+++ Projects/Food_Order/backend/app.js
@@ -66,4 +66,10 @@ app.use((req, res) => {
   res.status(404).json({ message: 'Not found' });
 });
 
-app.listen(3000);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
